3f86522dc77c72d5c36fa6ba1eb41cd333677cc4
unknown <nmeloccaro892@wit-xdvm-008.MILLARDPS.NET>
2012-12-13 11:27:57 -0600

    Added new features

--- index.php
+++ index.php
@@ -12,29 +12,46 @@
 				<?php get_template_part('nav') // includes nav.php ?>
 			</nav>
 			<div  id="main">
-				<?php 
-				/**
-				 * Check to see if the current request...
-				 * is_catergory()
-				 * is_front_page()
-				 * is_home
-				 * is_404()
-				 * is_archive
-				 * is_page()
-				 * is_single() --> e.g. a single post
-				 * is_tag()
-				 * is_search() --> a page display search results
-				 */
-				if (is_front_page()) {
-					
-					// Include filr named content-home.php
-					get_template_part('content','home');
-				} elseif (is_page() || is_single()) {
-					
-					//Include file name content-single.php
-					get_template_part('content','single');
-				}
-				?>
+				<div class="row">
+					<!-- Main Column -->
+					<div class="span9">
+						<?php 
+						/**
+						 * Check to see if the current request...
+						 * is_catergory()
+						 * is_front_page()
+						 * is_home
+						 * is_404()
+						 * is_archive
+						 * is_page()
+						 * is_single() --> e.g. a single post
+						 * is_tag()
+						 * is_search() --> a page display search results
+						 */
+						if (is_front_page()) {
+							
+							// Include filr named content-home.php
+							get_template_part('content','home');
+						} elseif (is_page() || is_single()) {
+							
+							//Include file name content-single.php
+							get_template_part('content','single');
+						} elseif (is_search()) {
+							
+							// Include file named content-search.php
+							get_template_part('content','search');
+						} else { //DIsplay Posts!
+							
+							// Include file named content-blog.php
+							get_template_part('content','blog');
+						}
+						?>
+					</div>
+					<!-- Right Sidebar -->
+					<div class="span3">         
+						<?php get_sidebar()?>
+					</div>
+				</div>
 			</div>
 			<footer>
 				<?php get_footer() ?>
